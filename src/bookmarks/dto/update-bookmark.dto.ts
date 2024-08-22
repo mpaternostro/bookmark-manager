f9b2d6593dbb8c72d4a7bf0d9d3830072cb85b0b
Marcelo <marcelodpaternostro@gmail.com>
2024-08-22 20:29:30 +0000

    fix: update bookmark always send title and url

--- src/bookmarks/dto/update-bookmark.dto.ts
+++ src/bookmarks/dto/update-bookmark.dto.ts
@@ -1,11 +1,9 @@
 import { z } from 'zod';
 
-export const updateBookmarkSchema = z
-  .object({
-    url: z.string().url(),
-    title: z.string(),
-    description: z.string(),
-  })
-  .partial();
+export const updateBookmarkSchema = z.object({
+  url: z.string().url(),
+  title: z.string(),
+  description: z.string().optional(),
+});
 
 export type UpdateBookmarkDto = z.infer<typeof updateBookmarkSchema>;
